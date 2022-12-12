a0e85b856b406eb91731f17daa92b4f18e0b490f
Rin Kuryloski <kuryloskip@vmware.com>
2022-12-12 09:15:02 +0100

    Remove unused Dir arg in erl_attrs_to_json.sh

--- gazelle/erl_attrs_to_json.sh
+++ gazelle/erl_attrs_to_json.sh
@@ -27,41 +27,41 @@ parse_json_string(Line) ->
             end
     end.
 
-record_attr(E, _Dir, behavior, Dep) ->
+record_attr(E, behavior, Dep) ->
     ets:insert(E, {behaviour, Dep});
-record_attr(E, _Dir, behaviour, Dep) ->
+record_attr(E, behaviour, Dep) ->
     ets:insert(E, {behaviour, Dep});
-%% record_attr(E, _Dir, compile, {parse_transform, Dep}) ->
+%% record_attr(E, compile, {parse_transform, Dep}) ->
 %%     ets:insert(E, {Dep});
-%% record_attr(E, _Dir, compile, Opts) when is_list(Opts) ->
+%% record_attr(E, compile, Opts) when is_list(Opts) ->
 %%     case proplists:get_value(parse_transform, Opts) of
 %%         undefined -> ok;
 %%         Dep -> ets:insert(E, {Dep})
 %%     end;
-record_attr(E, _Dir, include, Path) ->
+record_attr(E, include, Path) ->
     ets:insert(E, {include, Path});
-record_attr(E, _Dir, include_lib, Path) ->
+record_attr(E, include_lib, Path) ->
     ets:insert(E, {include_lib, Path});
-record_attr(_, _, _, _) ->
+record_attr(_, _, _) ->
     ok.
 
-walk_forms(E, Dir, Fd, StartLocation) ->
+walk_forms(E, Fd, StartLocation) ->
     case io:parse_erl_form(Fd, undefined, StartLocation) of
         {ok, AbsData, EndLocation} ->
             case AbsData of
                 {attribute, _, Key, Value} ->
                     % io:format(standard_error, "AbsData: ~p~n", [AbsData]),
-                    record_attr(E, Dir, Key, Value),
-                    walk_forms(E, Dir, Fd, EndLocation);
+                    record_attr(E, Key, Value),
+                    walk_forms(E, Fd, EndLocation);
                 _ ->
-                    walk_forms(E, Dir, Fd, EndLocation)
+                    walk_forms(E, Fd, EndLocation)
             end;
         {eof, _} ->
             file:close(Fd);
         {error, _} ->
             file:close(Fd);
         {error, _, ErrorLocation} ->
-            walk_forms(E, Dir, Fd, ErrorLocation)
+            walk_forms(E, Fd, ErrorLocation)
     end.
 
 deps(E) ->
@@ -88,7 +88,7 @@ parse(File) ->
 
     case file:open(File, [read]) of
         {ok, Fd} ->
-            walk_forms(E, filename:dirname(File), Fd, 0),
+            walk_forms(E, Fd, 0),
             Map = deps(E),
             %% io:format(standard_error, "Map: ~p~n", [Map]),
             to_json(Map);
