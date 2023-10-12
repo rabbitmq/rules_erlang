375e034ebc232361bbb3d6ed160f695f7f6a5f9c
Rin Kuryloski <kuryloskip@vmware.com>
2023-10-12 15:46:55 +0200

    coverdata_to_lcov: update source file location inference

--- tools/coverdata_to_lcov/src/coverdata_to_lcov.erl
+++ tools/coverdata_to_lcov/src/coverdata_to_lcov.erl
@@ -10,7 +10,9 @@ main([TestName, CoverdataFile, LcovFile]) ->
     ScriptName = filename:basename(escript:script_name()),
     io:format(standard_error, "~s: converting ~s to lcov...~n",
               [ScriptName, CoverdataFile]),
+
     {ok, Cwd} = file:get_cwd(),
+    {ok, ExecRoot} = find_execroot(Cwd),
 
     ok = cover:import(CoverdataFile),
     Modules = cover:imported_modules(),
@@ -19,8 +21,8 @@ main([TestName, CoverdataFile, LcovFile]) ->
     io:format(S, "TN:~s~n", [TestName]),
     lists:foreach(
       fun (Module) ->
-              Path = code:which(Module),
-              io:format(S, "SF:~s~n", [Path]),
+              {ok, SF} = guess_source_file(Module, ExecRoot),
+              io:format(S, "SF:~s~n", [SF]),
 
               {ok, Lines} = cover:analyse(Module, calls, line),
               {LH, LF} = lists:foldl(
@@ -29,10 +31,9 @@ main([TestName, CoverdataFile, LcovFile]) ->
                                        0 ->
                                            ok;
                                        _ ->
-                                           RelPath = relative_path(Path, Cwd),
                                            io:format(standard_error,
                                                      "~s: ~s:~B calls ~B~n",
-                                                     [ScriptName, RelPath, N, Calls])
+                                                     [ScriptName, SF, N, Calls])
                                    end,
                                    io:format(S, "DA:~B,~B~n", [N, Calls]),
                                    {LinesHit + is_hit(Calls), LinesFound + 1}
@@ -43,10 +44,32 @@ main([TestName, CoverdataFile, LcovFile]) ->
       end, Modules),
     file:close(S).
 
-relative_path(Path, Cwd) ->
-    case string:prefix(Path, Cwd ++ "/") of
-        nomatch -> Path;
-        RelPath -> RelPath
+find_execroot(Dir) ->
+    find_execroot(Dir, filename:dirname(Dir)).
+
+find_execroot("/", "/") ->
+    not_found;
+find_execroot(Dir, Parent) ->
+    case {filename:basename(Dir), filename:basename(Parent)} of
+        {"_main", "execroot"} ->
+            {ok, Dir};
+        _ ->
+            find_execroot(filename:dirname(Dir), filename:dirname(Parent))
+    end.
+
+guess_source_file(Module, SourceRoot) ->
+    case source_file(Module, SourceRoot, "apps") of
+        not_found ->
+            source_file(Module, SourceRoot, "deps");
+        P ->
+            P
+    end.
+
+source_file(Module, SourceRoot, AppsDir) ->
+    Pattern = filename:join([AppsDir, "*", "src", atom_to_list(Module) ++ ".erl"]),
+    case filelib:wildcard(Pattern, SourceRoot) of
+        [Path] -> {ok, Path};
+        _ -> not_found
     end.
 
 is_hit(0) ->
