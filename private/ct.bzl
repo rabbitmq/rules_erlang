7002d071a13fe5abeebd945090cc203cb0ec6a8a
Rin Kuryloski <kuryloskip@vmware.com>
2023-10-12 14:48:53 +0200

    Convert common_test coverage data to lcov for bazel

--- private/ct.bzl
+++ private/ct.bzl
@@ -191,7 +191,8 @@ if [ -n "${{COVERAGE}}" ]; then
     "{erlang_home}"/bin/escript $TEST_SRCDIR/$TEST_WORKSPACE/{coverdata_to_lcov} \\
         {suite_name} \\
         ${{COVERAGE_OUTPUT_FILE}} \\
-        ${{COVERAGE_OUTPUT_FILE}}
+        ${{COVERAGE_OUTPUT_FILE}} \\
+        > ${{TEST_UNDECLARED_OUTPUTS_DIR}}/coverdata_to_lcov.log
 fi
 """.format(
             maybe_install_erlang = maybe_install_erlang(ctx, short_path = True),
--- tools/coverdata_to_lcov/src/coverdata_to_lcov.erl
+++ tools/coverdata_to_lcov/src/coverdata_to_lcov.erl
@@ -7,26 +7,49 @@
 -spec main([string()]) -> no_return().
 
 main([TestName, CoverdataFile, LcovFile]) ->
+    ScriptName = filename:basename(escript:script_name()),
     io:format(standard_error, "~s: converting ~s to lcov...~n",
-              [filename:basename(escript:script_name()), CoverdataFile]),
+              [ScriptName, CoverdataFile]),
+    {ok, Cwd} = file:get_cwd(),
+
     ok = cover:import(CoverdataFile),
     Modules = cover:imported_modules(),
-    {result, Ok, _Fail} = cover:analyse(Modules, calls, line),
     
     {ok, S} = file:open(LcovFile, [write]),
     io:format(S, "TN:~s~n", [TestName]),
-    %% lists:foreach(
-    %%   fun (Module) ->
-    %%           {ok, Lines} = cover:analyse(Module, calls, line),
-    %%           lists:foreach(
-    %%             fun ({{M, N}, Calls}) ->
-    %%                     io:format(standard_error, "\t~p~n", [{M, N, Calls}])
-    %%             end, Lines)
-    %%   end, Modules),
     lists:foreach(
-      fun
-          ({{M, N} = _Line, Calls}) ->
-              io:format(standard_error, "\t~p~n", [{M, N, Calls}])
-      end, Ok),
-    file:close(S),
-    halt(1).
+      fun (Module) ->
+              Path = code:which(Module),
+              io:format(S, "SF:~s~n", [Path]),
+
+              {ok, Lines} = cover:analyse(Module, calls, line),
+              {LH, LF} = lists:foldl(
+                           fun ({{_M, N}, Calls}, {LinesHit, LinesFound}) ->
+                                   case Calls of
+                                       0 ->
+                                           ok;
+                                       _ ->
+                                           RelPath = relative_path(Path, Cwd),
+                                           io:format(standard_error,
+                                                     "~s: ~s:~B calls ~B~n",
+                                                     [ScriptName, RelPath, N, Calls])
+                                   end,
+                                   io:format(S, "DA:~B,~B~n", [N, Calls]),
+                                   {LinesHit + is_hit(Calls), LinesFound + 1}
+                           end, {0, 0}, Lines),
+              io:format(S, "LH:~B~n", [LH]),
+              io:format(S, "LF:~B~n", [LF]),
+              io:format(S, "end_of_record~n", [])
+      end, Modules),
+    file:close(S).
+
+relative_path(Path, Cwd) ->
+    case string:prefix(Path, Cwd ++ "/") of
+        nomatch -> Path;
+        RelPath -> RelPath
+    end.
+
+is_hit(0) ->
+    0;
+is_hit(_) ->
+    1.
