e5fecb0c2772a6b1bfb1579ae5ab2c6b9becc88a
Rin Kuryloski <kuryloskip@vmware.com>
2023-12-06 10:06:06 +0100

    Extract another module

--- test/rules_erlang_compiler/test/rules_erlang_compiler_SUITE.erl
+++ test/rules_erlang_compiler/test/rules_erlang_compiler_SUITE.erl
@@ -6,25 +6,8 @@
 -compile(export_all).
 
 all() -> [
-          consume_to_list,
           transform_erlc_opts
          ].
-
-consume_to_list(_) ->
-    G1 = digraph:new([acyclic]),
-
-    Root = digraph:add_vertex(G1, "root"),
-    Middle = digraph:add_vertex(G1, "middle"),
-    Leaf1 = digraph:add_vertex(G1, "leaf1"),
-    Leaf2 = digraph:add_vertex(G1, "leaf2"),
-    Loner = digraph:add_vertex(G1, "loner"),
-
-    digraph:add_edge(G1, Root, Middle),
-    digraph:add_edge(G1, Middle, Leaf1),
-    digraph:add_edge(G1, Middle, Leaf2),
-
-    ?assertEqual([Root, Middle, Leaf1, Leaf2, Loner],
-                 rules_erlang_compiler:consume_to_list(G1)).
 
 transform_erlc_opts(_) ->
     ?assertEqual([warnings_as_errors,deterministic,debug_info,
--- tools/rules_erlang_compiler/src/rules_erlang_compiler.erl
+++ tools/rules_erlang_compiler/src/rules_erlang_compiler.erl
@@ -6,7 +6,6 @@
 
 -ifdef(TEST).
 -export([
-         consume_to_list/1,
          transform_erlc_opts/1
         ]).
 -endif.
@@ -143,7 +142,7 @@ execute(#{arguments := #{targets_file := ConfigJsonPath}}) ->
             % we might also have to write this to a file so that dialyze rules can use it?
             %% io:format(standard_error, "TargetsWithDeps: ~p~n", [TargetsWithDeps]),
 
-            AppCompileOrder = consume_to_list(G),
+            AppCompileOrder = digraph_tools:consume_to_list(G),
 
             %% io:format(standard_error, "Compilation Order: ~p~n", [AppCompileOrder]),
 
@@ -398,7 +397,7 @@ compile(AppName,
     G = src_graph(AppName, Suffix, Analysis, CopiedSrcs, ModuleIndex, AFCT),
     %% io:format(standard_error, "~p: ~p~n", [AppName, G]),
 
-    Srcs = consume_to_list(G),
+    Srcs = digraph_tools:consume_to_list(G),
     %% io:format(standard_error, "~p: ~p~n", [AppName, Srcs]),
 
     {ok, OldCwd} = file:get_cwd(),
@@ -448,40 +447,6 @@ compile(AppName,
           end, {ok, 0, []}, Srcs),
     ok = file:set_cwd(OldCwd),
     R.
-
--spec consume_to_list(digraph:graph()) -> [digraph:vertex()].
-consume_to_list(G) ->
-    consume_to_list(G, []).
-
-consume_to_list(G, Acc) ->
-    case lists:sort(digraph:vertices(G)) of
-        [] ->
-            digraph:delete(G),
-            Acc;
-        [V | _]  ->
-            R = find_root(G, V),
-            digraph:del_vertex(G, R),
-            consume_to_list(G, Acc ++ [R])
-    end.
-
-find_root(G, V) ->
-    Edges = lists:sort(
-              fun (E1, E2) ->
-                      {E1, P1, V, _} = digraph:edge(G, E1),
-                      {E2, P2, V, _} = digraph:edge(G, E2),
-                      case lists:sort([P1, P2]) of
-                          [P1, P2] -> true;
-                          [P2, P1] -> false
-                      end
-              end,
-              digraph:in_edges(G, V)),
-    case Edges of
-        [] ->
-            V;
-        [E | _] ->
-            {E, P, V, _} = digraph:edge(G, E),
-            find_root(G, P)
-    end.
 
 -spec string_to_term(string()) -> term().
 string_to_term(S) ->
